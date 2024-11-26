abec2648eb412eda5b56328f3ff53f0d31e7d928
yoan.zhang <Yoan.Zhang@x-humanoid.com>
2024-11-26 13:58:31 +0800

    update deploy code

--- deploy_act_franka_3rgb.sh
+++ deploy_act_franka_3rgb.sh
@@ -1,16 +1,24 @@
 ######################
 
-export HF_HOME=/media/users/wk/huggingface_model
-export TORCH_HOME=/media/users/wk/torch_model
+# export HF_HOME=/media/users/wk/huggingface_model
+# export TORCH_HOME=/media/users/wk/torch_model
+
+export HF_HOME=/home/ps/wk/cache_model/huggingface_model
+export TORCH_HOME=/home/ps/wk/cache_model/torch_model
+
+# agent_best
+# policy_last
+# num_steps is used for training
 
 python3 deploy_algo.py \
-        --camera_names camera_left camera_right camera_top \
-        --ckpt_dir ./ckpt_dir/ACT_camlrt/241121/table/place_in_bread_on_plate_1_lr1e5_batch24_chunk50 \
+        --task_name franka_3rgb_241021_insert_marker_1 \
+        --camera_names left right top \
+        --ckpt_dir /media/ps/wk/benchmark_results/act/franka_3rgb_241021_insert_marker_1/ckpt \
         --exp_type franka_3rgb \
-        --ckpt_name agent_last.ckpt \
+        --ckpt_name policy_last.ckpt \
         --agent_class ACT \
         --chunk_size 50 --hidden_dim 512 --dim_feedforward 3200 \
         --kl_weight 10 \
         --backbone 'resnet18' \
         --act_norm_class norm2 \
-        --temporal_agg \
+        --temporal_agg --num_steps 0 \
